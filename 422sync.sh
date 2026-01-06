60d53af95473f111bba453ae1d28e4bd8f3e35ef
Jerry Li <jerryzli@Jerrys-MacBook-Pro.local>
2026-01-06 13:59:06 -0800

    update first week

--- 422sync.sh
+++ 422sync.sh
@@ -1,2 +1,1 @@
-scp -r cse422wi25/* jerryzli@bicycle.cs.washington.edu:/cse/web/courses/cse422/25wi/cse422wi25
-scp cse422wi25.html jerryzli@bicycle.cs.washington.edu:/cse/web/courses/cse422/25wi/cse422wi25.html
+scp -r cse422wi26.html lectures scribe_notes files hw jerryzli@bicycle.cs.washington.edu:/cse/web/courses/cse422/26wi/
